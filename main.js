4ef42dec6aaa1f49481f96989e01b08c44527434
kumaranil3921 <kumaranil3921@gmail.com>
2017-01-10 00:18:25 +0530

    read from Database

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var http = require("http");
+//var http = require("http");
 var mysql      = require('mysql');
 /*http.createServer(function (request, response) {
 
-   // Send the HTTP header 
+   // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   
+
    // Send the response body as "Hello World"
    response.end('Hello World\n');
 }).listen(8081);*/
@@ -16,14 +16,22 @@ var connection = mysql.createConnection({
   password : 'root',
   database : 'mydb'
 });
- 
-connection.connect();
- 
-connection.query('create table users(userID int, userName varchar(100))', function(err, rows, fields) {
-  if (err) throw err;
- 
-  console.log('The solution is: ', rows[0].solution);
-});
 
+connection.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+connection.query('SELECT * FROM student',function(err,rows){
+  if(err) throw err;
+
+  console.log('Data received from Db:\n');
+  console.log(rows);
+});
+connection.end(function(err) {
+
+});
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+//console.log('Server running at http://127.0.0.1:8081/');
